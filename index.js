b8efc503b77b6a107eac674a6ee642a9bba38542
kasperfryd <kfry@techcollege.dk>
2024-12-10 14:44:34 +0100

    updated url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5500",
+    origin: "https://iridescent-entremet-a17b07.netlify.app",
   },
 });
 
